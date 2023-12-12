f6926f4c5fe739c63a1720222d2d04bf0e9be3b6
kouseina <daffa@diyo.app>
2023-12-13 00:17:31 +0700

    feat(login): add login form validate

--- lib/modules/login_page.dart
+++ lib/modules/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_realm/modules/home_page.dart';
 import 'package:flutter_realm/widgets/main_textfield_widget.dart';
 
 class LoginPage extends StatefulWidget {
@@ -30,42 +31,56 @@ class _LoginPageState extends State<LoginPage> {
     passwordController.dispose();
   }
 
+  void onLogin() {
+    if (!_formKey.currentState!.validate()) return;
+
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Links"),
+        title: const Text("Login"),
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 35,
-                    ),
-                    MainTextFieldWidget(
-                      controller: emailController,
-                      labelText: "Email",
-                      textInputType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    MainTextFieldWidget(
-                      controller: passwordController,
-                      labelText: "Password",
-                      obscureText: true,
-                    ),
-                  ],
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MainTextFieldWidget(
+                  controller: emailController,
+                  labelText: "Email",
+                  textInputType: TextInputType.emailAddress,
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 16,
+                ),
+                MainTextFieldWidget(
+                  controller: passwordController,
+                  labelText: "Password",
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: onLogin,
+                    child: const Text("Login"),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
